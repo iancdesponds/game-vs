12cb3345235c33fefe4ae486ffc5b73e8a540e28
joaopgs4 <jpgardenalli@gmail.com>
2025-05-06 21:38:09 -0300

    Menu Animated

--- Assets/Scripts/Menu/ManagerMenu.cs
+++ Assets/Scripts/Menu/ManagerMenu.cs
@@ -2,11 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ManagerMenu : MonoBehaviour
+public class ManagerMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public Image backgroundImage;
+    public Sprite defaultBackground;      // "main_menu.png"
+    public Sprite playHoverBackground;    // "main_menu_play.png"
+
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        backgroundImage.sprite = playHoverBackground;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        backgroundImage.sprite = defaultBackground;
     }
 }
