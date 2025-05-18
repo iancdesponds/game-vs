9b2c1740d309e40a2de2d630973c828270c146e8
Benibergas <benis@al.insper.edu.br>
2025-05-18 16:15:06 -0300

    menu de level up criado

--- Assets/Scripts/Player/PlayerXP.cs
+++ Assets/Scripts/Player/PlayerXP.cs
@@ -1,23 +1,43 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerXP : MonoBehaviour
 {
-    public int maxXP = 100;
+    public int maxXP = 30;
     private int currentXP = 0;
 
     public XpBarUI xpBarUI;
+    public GameObject levelUpMenu; // atribua no Inspector
 
     void Start()
     {
         currentXP = 0;
         xpBarUI.SetMaxXP(maxXP);
         xpBarUI.SetXP(currentXP);
+        levelUpMenu.SetActive(false); // começa invisível
     }
 
     public void GainXP(int amount)
     {
         currentXP = Mathf.Min(currentXP + amount, maxXP);
+        xpBarUI.SetXP(currentXP);
+
+        if (currentXP >= maxXP)
+        {
+            TriggerLevelUp();
+        }
+    }
+
+    void TriggerLevelUp()
+    {
+        Time.timeScale = 0f; // pausa o jogo
+        levelUpMenu.SetActive(true); // mostra menu
+    }
+
+    public void ConfirmUpgrade()
+    {
+        levelUpMenu.SetActive(false);
+        Time.timeScale = 1f;
+        currentXP = 0;
         xpBarUI.SetXP(currentXP);
     }
 }
